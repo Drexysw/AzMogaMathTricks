8d2f76b876f11435873a8f603fb6927de736a5e3
Drexysw <knevdani@gmail.com>
2024-05-29 22:30:44 +0300

    declared a method for increase points and canplayermove which will indicate if a player has lost or can continue also initilized the player class

---
+++ AzMogaMathTricks/GameObjects/Poll.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzMogaMathTricks.GameObjects
+{
+    internal class Poll
+    {
+        protected int CurrentRowPos  { get; set; }
+        protected int CurrentColPos { get; set; }
+    }
+}
